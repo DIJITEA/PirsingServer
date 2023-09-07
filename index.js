4d31db196e0ba15df252b076dd50e8711f0ce0b8
DESKTOP-SHK9PBH\olegk <kornyakovo@gmail.com>
2023-09-07 05:30:44 +0400

    CORS4

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const app = express();
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://DIJITEA.github.io/PirsingLight/');
+  res.setHeader('Access-Control-Allow-Origin', 'https://dijitea.github.io');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
